63516c0b58cfaa57656bae57ff9878592d537d22
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-06-26 16:15:53 +0700

    create animation class

---
+++ lib/company_profile/anim/company_details_intro_animation.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+//todo 1 (finish)
+class CompanyDetailsIntroAnimation {
+
+  AnimationController controller;
+  Animation<double> bgDropOpacity;
+  Animation<double> bgDropBlur;
+
+// Animation<double> avatarSize;
+// Animation<double> nameOpacity;
+// Animation<double> locationOpacity;
+// Animation<double> dividerWidth;
+// Animation<double> aboutOpacity;
+// Animation<double> courseScrollerXTranslation;
+// Animation<double> courseScrollerOpacity;
+
+
+  CompanyDetailsIntroAnimation({
+    required this.controller,
+  })  : bgDropOpacity = Tween(begin: 0.5, end: 1.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.000, 0.500, curve: Curves.ease),
+          ),
+        ),
+        bgDropBlur = Tween(begin: 0.0, end: 5.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.000, 0.800, curve: Curves.ease),
+          ),
+        );
+}

fc06cb6ca5a81a5ca97ba52fc0616da4f43b3916
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-22 10:54:59 +0700

    decorated box transition

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,7 @@ import 'package:animaton_with_flutter/animated_widget_example.dart';
 import 'package:animaton_with_flutter/company_profile/ui/company_details_animator.dart';
 import 'package:animaton_with_flutter/company_profile/ui/company_details_page.dart';
 import 'package:animaton_with_flutter/counter_animation.dart';
+import 'package:animaton_with_flutter/decorated_box_transition_example.dart';
 import 'package:animaton_with_flutter/home_chain_animation.dart';
 import 'package:animaton_with_flutter/tween_animation.dart';
 import 'package:flutter/material.dart';
@@ -56,186 +57,202 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 20 * animation.value,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontSize: 20 * animation.value,
+                  ),
                 ),
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Counter Animation'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CounterAnimation(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Tween Animation'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TweenAnimationExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Chain Animation'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeChainAnimation(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Example project'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CompanyDetailsAnimator(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated align'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedAlignExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated builder'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedBuilderExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated container'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedContainerExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated cross fade'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedCrossFadeExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated text'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DefaultTextStyleTransitionExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated list'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedListSample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated opacity'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LogoFade(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated positioned'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedPositionedExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated size'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedSizeExample(),
-                    ),
-                  );
-                },
-              ),
-              MaterialButton(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text('Animated widget'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AnimatedWidgetExample(),
-                    ),
-                  );
-                },
-              ),
-            ],
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Counter Animation'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CounterAnimation(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Tween Animation'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TweenAnimationExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Chain Animation'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeChainAnimation(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Example project'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CompanyDetailsAnimator(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated align'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedAlignExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated builder'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedBuilderExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated container'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedContainerExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated cross fade'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedCrossFadeExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated text'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DefaultTextStyleTransitionExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated list'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedListSample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated opacity'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LogoFade(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated positioned'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedPositionedExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated size'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedSizeExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Animated widget'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnimatedWidgetExample(),
+                      ),
+                    );
+                  },
+                ),
+                MaterialButton(
+                  color: Theme.of(context).colorScheme.primary,
+                  child: Text('Decorated box transition'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DecoratedBoxTransitionExampleApp(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
